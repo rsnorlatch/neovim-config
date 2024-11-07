b26e0e44ee21ac4e367b3279bfe8f2d6ec518f44
rsnorlatch <muhfarisnew@gmail.com>
2024-11-07 09:15:52 +0700

    changed line unselected line number color to make it easier to see

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,12 +3,14 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'stevearc/oil.nvim',
-  opts = {},
+  {
+    'stevearc/oil.nvim',
+    opts = {},
 
-  config = function()
-    vim.keymap.set('n', '<leader>e', '<C-w>s<C-w>T<cmd>Oil<cr>', { desc = 'Opens Oil explorer on the right side' })
-  end,
+    config = function()
+      vim.keymap.set('n', '<leader>e', '<C-w>s<C-w>T<cmd>Oil<cr>', { desc = 'Opens Oil explorer on the right side' })
+    end,
 
-  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  },
 }
