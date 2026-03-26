b1d2efb1a0361ae4f5fed1747bfb492876218596
rsnorlatch <muhfarisnew@gmail.com>
2026-03-26 19:14:43 +0700

    added easy dotnet

--- lua/custom/plugins/util.lua
+++ lua/custom/plugins/util.lua
@@ -33,4 +33,11 @@ return {
       },
     },
   },
+  {
+    'GustavEikaas/easy-dotnet.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('easy-dotnet').setup()
+    end,
+  },
 }
