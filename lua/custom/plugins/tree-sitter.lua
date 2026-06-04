b39496dd27c963e5e22ae9be167e7833e61cd334
rsnorlatch <gdm@GDM.localdomain>
2026-06-04 13:44:56 +0700

    changed treesitter branch to master

--- lua/custom/plugins/tree-sitter.lua
+++ lua/custom/plugins/tree-sitter.lua
@@ -3,6 +3,8 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     lazy = false,
+    branch = 'master',
+    main = 'nvim-treesitter.configs',
     config = function()
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(ev)
